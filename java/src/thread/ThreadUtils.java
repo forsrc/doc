7b72148e8314fdf0c8638041d42e209267141159
yes <forsrc@gmail.com>
2017-08-21 20:07:22 +0900

    Update ThreadUtils.java

--- java/src/thread/ThreadUtils.java
+++ java/src/thread/ThreadUtils.java
@@ -9,26 +9,22 @@ import java.util.concurrent.TimeUnit;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.MoreExecutors;
 
-
 public class ThreadUtils {
 
-    private static final int SECONDS_TO_RUN = 1;
+    // private static final int SECONDS_TO_RUN = 1;
 
-    private static final int RECORDS_PER_SECOND = 10000;
+    // private static final int RECORDS_PER_SECOND = 10000;
 
     private static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(1);
 
     public static void main(String[] args) throws InterruptedException {
-        final AtomicLong sequenceNumber = new AtomicLong(0);
-        final AtomicLong completed = new AtomicLong(0);
-        final FutureCallback<String> callback = new FutureCallback<String>() {
+        run(1, 1000, new FutureCallback<String>() {
             @Override
             public void onFailure(Throwable t) {
                 System.out.println(t.getMessage());
@@ -36,8 +32,34 @@ public class ThreadUtils {
 
             @Override
             public void onSuccess(String result) {
+                // System.out.println(t.getMessage());
+            }
+        }, new Callable<String>() {
+
+            @Override
+            public String call() throws Exception {
+
+                return System.currentTimeMillis() + "";
+            }
+
+        });
+    }
+
+    public static <T> void run(int secondsToRun, int pecordsPerSecond, FutureCallback<T> callback, Callable<T> callable)
+            throws InterruptedException {
+        final AtomicLong sequenceNumber = new AtomicLong(0);
+        final AtomicLong completed = new AtomicLong(0);
+        final FutureCallback<T> futureCallback = new FutureCallback<T>() {
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println(t.getMessage());
+                callback.onFailure(t);
+            }
+
+            @Override
+            public void onSuccess(T result) {
+                callback.onSuccess(result);
                 completed.getAndIncrement();
-                //System.out.println(result);
             }
         };
 
@@ -46,48 +68,38 @@ public class ThreadUtils {
         final Runnable putOneRecord = new Runnable() {
             @Override
             public void run() {
-
-                // TIMESTAMP is our partition key
-                ListenableFuture<String> f = executorService.submit(new Callable<String>() {
-                    public String call() {
-                        return "" + System.currentTimeMillis();
-                    }
-                });
-
-                Futures.addCallback(f, callback);
+                ListenableFuture<T> f = executorService.submit(callable);
+                Futures.addCallback(f, futureCallback);
             }
         };
 
         EXECUTOR.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                printRate(sequenceNumber, completed);
+                printRate(secondsToRun, pecordsPerSecond, sequenceNumber, completed);
             }
         }, 100, 100, TimeUnit.MILLISECONDS);
 
-        executeAtTargetRate(EXECUTOR, putOneRecord, sequenceNumber, SECONDS_TO_RUN, RECORDS_PER_SECOND);
+        executeAtTargetRate(EXECUTOR, putOneRecord, sequenceNumber, secondsToRun, pecordsPerSecond);
 
-        EXECUTOR.awaitTermination(SECONDS_TO_RUN + 1, TimeUnit.SECONDS);
-        printRate(sequenceNumber, completed);
+        EXECUTOR.awaitTermination(secondsToRun + 1, TimeUnit.SECONDS);
+        printRate(secondsToRun, pecordsPerSecond, sequenceNumber, completed);
         System.out.println("Finished.");
     }
 
-    private static void printRate(AtomicLong sequenceNumber, AtomicLong completed) {
+    private static void printRate(int secondsToRun, int pecordsPerSecond, AtomicLong sequenceNumber,
+            AtomicLong completed) {
         long put = sequenceNumber.get();
-        long total = RECORDS_PER_SECOND * SECONDS_TO_RUN;
+        long total = pecordsPerSecond * secondsToRun;
         double putPercent = 100.0 * put / total;
         long done = completed.get();
         double donePercent = 100.0 * done / total;
-        System.out.println(String.format("--> %d of %d so far (%.2f %%), %d have completed (%.2f %%)", put,
-                total, putPercent, done, donePercent));
+        System.out.println(String.format("--> %d of %d so far (%.2f %%), %d have completed (%.2f %%)", put, total,
+                putPercent, done, donePercent));
     }
 
-    private static void executeAtTargetRate(
-            final ScheduledExecutorService exec,
-            final Runnable task,
-            final AtomicLong counter,
-            final int durationSeconds,
-            final int ratePerSecond) {
+    private static void executeAtTargetRate(final ScheduledExecutorService exec, final Runnable task,
+            final AtomicLong counter, final int durationSeconds, final int ratePerSecond) {
 
         exec.scheduleWithFixedDelay(new Runnable() {
             final long startTime = System.nanoTime();
